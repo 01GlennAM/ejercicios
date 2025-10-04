39c2e641ff9b6996ee6600d8be778398f53d4d8f
glenn <glennalverez@gmail.com>
2025-10-03 19:30:23 -0500

    ejercicios actualizados

--- src/main/java/com/example/TrasportePublico.java
+++ src/main/java/com/example/TrasportePublico.java
@@ -13,29 +13,67 @@ public class TrasportePublico {
 
     Scanner sc = new Scanner (System.in);
 
+    
+    int tiempoToltal = 0;
     int bus = 20;
     int metro = 15;
-    int caminar = 10;
-  
+    int caminar =10;
+    int tiempoToltalTraficoPesado = 0;
+    int tiempoToltalBajoLluvia = 0;
+    double trafico = 1.5;
+    double lluvia = 2;
+
+
+    // tiempo en bus  20 minutos
     System.out.println("¿Vas a tomar el bus? (si/no): ");
     String busTiempo = sc.nextLine();
+    if (busTiempo.equals("si")) {
+      tiempoToltal += bus;
 
+    }
+  
+    //tiempo en metro 15m
     System.out.println("¿Vas a tomar el metro? (si/no): ");
     String metroTiempo = sc.nextLine();
-
-    System.out.println("¿Vas a caminar? (si/no): ");
-    String caminarTiempo = sc.nextLine();
-
-    if (busTiempo.equals("si") || metroTiempo.equals("si") || caminarTiempo.equals("si")) {
-      System.out.println("el tiempo es del bus es de " + bus + metro + caminar + "") ;
+    if (metroTiempo.equals("si")) {
+      tiempoToltal += metro;
       
     }
-
-   
-      
-    }
-
     
 
-} 
+    // tiempo caminando 10m
+    System.out.println("¿Vas a caminar? (si/no): ");
+    String caminarTiempo = sc.nextLine();
+    if (caminarTiempo.equals("si")) {
+      tiempoToltal += caminar;
+    }
+    
+  
+    // si hay trafico se suma el 50%
+    System.out.println("¿hay trafico pesado? (si/no): ");
+    String traficoPesado = sc.nextLine();
+    if (traficoPesado.equals("si") && busTiempo.equals("si")) {
+      tiempoToltalTraficoPesado += bus * trafico;
+       System.out.println("si hay trafico pesado en el bus te demoras  " + (tiempoToltal + tiempoToltalTraficoPesado) + " minutos mas"); 
+    }
+
+     // si hay lluvia y camina entonces 100% 
+    System.out.println("¿hay lluvia? (si/no): ");
+    String lluviaTiempo = sc.nextLine();
+    if (lluviaTiempo.equals("si") && caminarTiempo.equals("si")) {
+      tiempoToltalBajoLluvia += caminar * lluvia;
+       System.out.println("si te toca caminar  " + (tiempoToltal +tiempoToltalBajoLluvia) + " minutos"); 
+
+     
+
+    }
+      
+  System.out.println("el tiempo total es de  " + tiempoToltal + " minutos en tiempo normal");
+ 
+
+  }
+}
+  
+
+
 
